315734c8b9778da75e3299e90f9446c28a117f45
Hui Dong <dreamdonghui@163.com>
2020-08-11 22:03:41 +0800

    V0.0.0.0  remove and recreated the project to solve the mess up.

--- Waypoints/Form1.cs
+++ Waypoints/Form1.cs
@@ -8,14 +8,18 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Waypoints_Caculator
+namespace Waypoints
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
 
+        private void MainForm_load(object sender, EventArgs e)
+        {
+            this.Text = "航路规划" + "V" + "0.0.0.0";
+        }
     }
 }
